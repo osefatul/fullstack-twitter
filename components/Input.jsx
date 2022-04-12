a637cc8c952f421e84b2ceb84b0e563bf30e81c0
osefatul <sefaullahomar2@gmail.com>
2022-04-12 04:02:32 -0700

    Input component has bee created

---
+++ components/Input.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Input() {
+  return (
+    <div className="border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll scrollbar-hide">
+      <img
+        src="https://avatars.githubusercontent.com/u/67508976?v=4s"
+        alt=""
+        className="h-11 w-11 rounded-full xl:mr-2.5"
+      />
+    </div>
+  );
+}
+
+export default Input;
