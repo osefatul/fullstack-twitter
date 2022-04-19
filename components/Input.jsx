82df06bd0b971083b20248e74e5fe5d6f890deb6
osefatul <sefaullahomar2@gmail.com>
2022-04-18 22:19:49 -0700

    updataed input

--- components/Input.jsx
+++ components/Input.jsx
@@ -6,13 +6,25 @@ import {
   XIcon,
 } from "@heroicons/react/outline";
 import React, { useRef, useState } from "react";
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
 
 function Input() {
   const [input, setInput] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
+  const [showEmojis, setShowEmojis] = useState(false);
   const filePickerRef = useRef(null);
 
   const addImageToPost = {};
+
+  // Emoji library
+  const addEmoji = (e) => {
+    let sym = e.unified.split("-");
+    let codesArray = [];
+    sym.forEach((el) => codesArray.push("0x" + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setInput(input + emoji);
+  };
 
   return (
     <div className="border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll scrollbar-hide">
@@ -22,7 +34,7 @@ function Input() {
         className="h-11 w-11 rounded-full xl:mr-2.5 cursor-pointer"
       />
 
-      <div className="w-full divide-y divide-gray-700 ">
+      <div className="divide-y divide-gray-800 w-full">
         <div>
           <textarea
             className="bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px]"
@@ -60,6 +72,32 @@ function Input() {
                 onChange={addImageToPost}
               />
             </div>
+
+            <div className="icon rotate-90">
+              <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+
+            <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+              <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+
+            <div className="icon">
+              <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+
+            {showEmojis && (
+              <Picker
+                onSelect={addEmoji}
+                style={{
+                  position: "absolute",
+                  marginTop: "465px",
+                  marginLeft: -40,
+                  maxWidth: "320px",
+                  borderRadius: "20px",
+                }}
+                theme="dark"
+              />
+            )}
           </div>
         </div>
       </div>
