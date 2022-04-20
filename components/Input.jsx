8c1c58319cdd2cf190b296972f5328321c81b2e1
osefatul <sefaullahomar2@gmail.com>
2022-04-19 21:25:21 -0700

    input tweet button

--- components/Input.jsx
+++ components/Input.jsx
@@ -99,6 +99,14 @@ function Input() {
               />
             )}
           </div>
+
+          <button
+            className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+            disabled={!input && !selectedFile}
+            // onClick={sendPost}
+          >
+            Tweet
+          </button>
         </div>
       </div>
     </div>
