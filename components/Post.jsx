ddb1fc0672c0c9243983d4fdfa6a405e06ec53f5
osefatul <sefaullahomar2@gmail.com>
2022-04-24 16:21:09 -0700

    Updated Input

--- components/Post.jsx
+++ components/Post.jsx
@@ -11,6 +11,34 @@ function Post({ id, post, postPage }) {
           className="h-11 w-11 rounded-full mr-4"
         />
       )}
+
+      <div className="flex flex-col space-y-2 w-full">
+        <div className={`flex ${!postPage && "justify-between"}`}>
+          {postPage && (
+            <img
+              src={post?.userImg}
+              alt="Profile Pic"
+              className="h-11 w-11 rounded-full mr-4"
+            />
+          )}
+          <div className="text-[#63767d]">
+            <div className="inline-block group">
+              <h4
+                className={`font-bold text-[15px] sm:text-base text-[#d9d9d9] group-hover:underline ${
+                  !postPage && "inline-block"
+                }`}
+              >
+                {post?.username}
+              </h4>
+              <span
+                className={`text-sm sm:text-[15px] ${!postPage && "ml-1.5"}`}
+              >
+                {post?.tag}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
