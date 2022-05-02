25547cc19942572033915a6b99b497be77ae722e
osefatul <sefaullahomar2@gmail.com>
2022-05-02 01:05:23 -0700

    authNext updated

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -35,5 +35,10 @@ export default NextAuth({
   },
 
   // Add secret JWT in order to deploy it on vercel.
-  secret: process.env.JWT_SECRET,
+  jwt: {
+    encryption: true,
+    secret: process.env.JWT_SECRET,
+    signingKey: process.env.JWT_SIGNING_KEY,
+    encryptionKey: process.env.JWT_ENCRYPTION_KEY,
+  },
 });
